2df1d616a9d863e3757a9b27a0533309ce81191e
Samarth Kamble <kamble.samarth91@gmail.com>
2024-02-18 19:04:06 +0530

    find the assets and file folder struture

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,11 @@
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main className="root">
+      <div className="root-container">
+        <div className="wrapper">{children}</div>
+      </div>
+    </main>
+  );
+};
+
+export default Layout;
